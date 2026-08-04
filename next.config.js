d64945beaf3113a4adbb2bf16eca5f1b76837951
igetwifimoney <tygirbaughn@gmail.com>
2026-08-04 15:57:34 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
-  },
+    images: {
+          domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+    },
+    async rewrites() {
+          return [
+            { source: '/trading', destination: '/trading.html' },
+                ]
+    },
 }
 
 module.exports = nextConfig
