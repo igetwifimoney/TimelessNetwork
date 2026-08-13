593bb9b3554ba383759d7635bd5f4e59ed547777
igetwifimoney <tygirbaughn@gmail.com>
2026-08-13 11:40:15 -0600

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,20 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      images: {
-              domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
-      },
-      async rewrites() {
-              return {
-                        beforeFiles: [
-                            { source: '/', destination: '/chooser.html' },
-                                  ],
-                        afterFiles: [
-                            { source: '/trading', destination: '/trading.html' },
-                            { source: '/network', destination: '/' },
-                                  ],
-                        fallback: [],
-              }
-      },
+  images: {
+    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        { source: '/', destination: '/chooser.html' },
+      ],
+      afterFiles: [
+        { source: '/trading', destination: '/trading.html' },
+        { source: '/account', destination: '/account.html' },
+        { source: '/network', destination: '/' },
+      ],
+      fallback: [],
+    }
+  },
 }
 
 module.exports = nextConfig
